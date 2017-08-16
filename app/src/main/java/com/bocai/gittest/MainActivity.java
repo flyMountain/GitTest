f87b5bc114ae1a6ff5d3a2993ceb75a9132126da
178731754@qq.com <dyf564985574>
2017-08-16 21:17:25 +0800

    Initial commit

---
+++ app/src/main/java/com/bocai/gittest/MainActivity.java
@@ -0,0 +1,18 @@
+package com.bocai.gittest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by yuanfei on 2017/8/16.
+ */
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ test/src/main/java/com/bocai/test/TestUtil.java
@@ -0,0 +1,8 @@
+package com.bocai.test;
+
+/**
+ * Created by yuanfei on 2017/8/16.
+ */
+
+public class TestUtil {
+}
